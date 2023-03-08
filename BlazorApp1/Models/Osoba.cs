261f90196e5f44c7a6150ea198bac22f8f12081d
VEDRAN <VEDRAN@DESKTOP-7FONVHN>
2023-03-08 18:37:40 +0100

    initial commit

---
+++ BlazorApp1/Models/Osoba.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApiPovezivanje.Models
+{
+    public class Osoba
+    {
+        public int Id { get; set; } 
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set;}
+        public string BrojMobitela { get; set; }
+        public string Grad { get; set; }
+        public string Adresa { get; set; }
+        public string Drzava { get; set; }
+
+    }
+}
